7f315786bea9aa6023d731f314c01097b91bab39
Bright1017 <melvinbright101@gmail.com>
2025-04-04 12:19:55 +0100

    modified serializers.py for users to manage their own activities

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
-from .models import User, Activity
+from .models import CustomUser, Activity
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = CustomUser.objects.create_user(**validated_data)
         return user
 
 class ActivitySerializer(serializers.ModelSerializer):
