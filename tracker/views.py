82272b9da6dbb5ee8e263b40625f83a890afde62
Bright1017 <melvinbright101@gmail.com>
2025-04-04 13:27:17 +0100

    gave authentication to users

--- tracker/views.py
+++ tracker/views.py
@@ -2,8 +2,17 @@ from rest_framework import viewsets, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from .models import User, Activity
-from .serializers import UserSerializer, ActivitySerializer
+from .serializers import UserSerializer, ActivitySerializer, ActivityHistorySerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import CustomUser, Activity
+from .permissions import IsOwnerOrAdmin, IsSelfOrAdmin
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import ActivityFilter
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
 
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
@@ -17,17 +26,45 @@ class CustomAuthToken(ObtainAuthToken):
             'user_id': user.pk,
             'email': user.email
         })
+    
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsSelfOrAdmin]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return CustomUser.objects.all()
+        return CustomUser.objects.filter(id=self.request.user.id)
+    
 
 class ActivityViewSet(viewsets.ModelViewSet):
     serializer_class = ActivitySerializer
-    permission_classes = [permissions.IsAuthenticated]
-
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ActivityFilter
+    
     def get_queryset(self):
-        return Activity.objects.filter(user=self.request.user)
+        queryset = Activity.objects.filter(user=self.request.user)
+        
+        # Handle date range filtering
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+        
+        if start_date and end_date:
+            queryset = queryset.filter(
+                date__date__range=[start_date, end_date]
+            )
+            
+        return queryset.order_by('-date')
+    
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+class ActivityHistoryView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        queryset = Activity.objects.filter(user=request.user)
+        filtered_queryset = ActivityFilter(request.GET, queryset=queryset).qs
+        serializer = ActivityHistorySerializer(filtered_queryset, many=True)
+        return Response(serializer.data)
