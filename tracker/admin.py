31f0f565c6be61ef3d7db9048f196a307300ce5e
Bright1017 <melvinbright101@gmail.com>
2025-04-04 12:00:22 +0100

    modified

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CustomUser, Activity
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+    list_filter = ['email']
+    search_fields = ['username']
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ['user', 'activity_type','duration','distance', 'distance_unit', 'calories', 'date']
+    list_filter = ['user', 'activity_type','duration','distance', 'distance_unit', 'calories', 'date']
+    search_fields = ['user', 'activity_type','duration','distance', 'distance_unit', 'calories', 'date']
--- tracker/models.py
+++ tracker/models.py
@@ -1,12 +1,42 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.conf import settings
 
 
 # Create your models here.
 
-class User(AbstractUser):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email,password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        if not username:
+            raise ValueError('Username is required')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+     
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(username, email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
     # Custom user model for potential future extensions
-    pass
+    email = models.EmailField(null=False, blank=False, unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = CustomUserManager()
+
 
 class Activity(models.Model):
     ACTIVITY_TYPES = [
@@ -21,13 +51,13 @@ class Activity(models.Model):
         ('mi', 'Miles'),
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     activity_type = models.CharField(max_length=3, choices=ACTIVITY_TYPES)
     duration = models.PositiveIntegerField()  # Minutes
     distance = models.FloatField(null=True, blank=True)
     distance_unit = models.CharField(max_length=2, choices=DISTANCE_UNITS, null=True, blank=True)
     calories = models.PositiveIntegerField()
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.get_activity_type_display()}"
